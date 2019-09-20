2921975e086d027c5bed85c6ec5224caa810d41b
gitLari <mrlari@hotmail.com>
2019-09-20 07:59:32 +0000

    mod textia3

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 echo "Hello, nice balls!\n\n";
-echo "https://github.com/armakuni/google-cloud-run-simplest-example on mallina";
+echo "\nhttps://github.com/armakuni/google-cloud-run-simplest-example on mallina";
+echo "\nJa nyky kama https://github.com/gitLari/malli";  
